6f4731ecd939ed6e2e838c282e0f462e91279e40
RYO1223 <kuwanor0811@gmail.com>
2025-06-01 15:44:42 +0900

    feat: アセットと多言語をビルドランナーに

--- Makefile
+++ Makefile
@@ -25,8 +25,7 @@ watch:
 
 .PHONY: localization
 localization:
-	dart run easy_localization:generate --source-dir ./assets/translations --output-dir ./lib/gen
-	dart run easy_localization:generate --source-dir ./assets/translations --output-dir ./lib/gen -f keys -o locale_keys.g.dart
+	dart run easy_localization_sheet
 
 .PHONY: unit-test
 unit-test:
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_practice_mvvm/app.dart';
-import 'package:flutter_practice_mvvm/gen/codegen_loader.g.dart';
 import 'package:flutter_practice_mvvm/utils/flavor.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -18,9 +17,8 @@ void main() async {
     ProviderScope(
       child: EasyLocalization(
         supportedLocales: const [Locale('en', 'US'), Locale('ja', 'JP')],
-        path: 'assets/translations',
         fallbackLocale: const Locale('en', 'US'),
-        assetLoader: const CodegenLoader(),
+        path: 'assets/translations',
         child: App(),
       ),
     ),
